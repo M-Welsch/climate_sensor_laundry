e66db28ac52c24eda3450552adcdd3db2d8a0ada
Maximilian Welsch <maxiwelsch@posteo.de>
2022-03-22 16:36:50 +0100

    add temperature sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,23 @@
 #include <Arduino.h>
+#include <Wire.h>
 #include <string.h>
 #include "display.h"
+#include "temperature_readout.h"
+
 
 Display display;
+Temperature_Sensor temperature_sensor;
 
 void setup(void) {
-    display.setup();
+  Wire.begin();
+  Serial.begin(9600);
+  display.setup();
+  temperature_sensor.setup();
 }
 
 void loop(void) {
-  float temperature = 21.123;
+  float temperature = temperature_sensor.get_probe_temperature_degrees();
   display.show_temperature(temperature);
+  Serial.printf("Temperature %f\n", temperature);
+  delay(1000);
 }
